fca945ef7232197e508d55b6509291530577d0b5
Mohamed amaan <mohamedamaan319@gmail.com>
2023-09-03 17:36:38 +0530

    Update index.ts

--- prisma/index.ts
+++ prisma/index.ts
@@ -1,21 +1,38 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
+
+// let prisma: PrismaClient;
+// declare global{
+//     namespace NodeJS{
+//         interface Global{
+//             prisma:PrismaClient
+//         }
+//     }
+// }
+
+// if(process.env.NODE_ENV === "production"){
+//     prisma = new PrismaClient();
+// }else{
+//     if (!global.prisma){
+//         global.prisma = new PrismaClient()
+//     }
+//     prisma = global.prisma
+// }
+
+// export default prisma
+import { PrismaClient } from '@prisma/client';
 
 let prisma: PrismaClient;
-declare global{
-    namespace NodeJS{
-        interface Global{
-            prisma:PrismaClient
-        }
-    }
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  let globalWithPrisma = global as typeof globalThis & {
+    prisma: PrismaClient;
+  };
+  if (!globalWithPrisma.prisma) {
+    globalWithPrisma.prisma = new PrismaClient();
+  }
+  prisma = globalWithPrisma.prisma;
 }
 
-if(process.env.NODE_ENV === "production"){
-    prisma = new PrismaClient();
-}else{
-    if (!global.prisma){
-        global.prisma = new PrismaClient()
-    }
-    prisma = global.prisma
-}
-
-export default prisma
+export default prisma;
